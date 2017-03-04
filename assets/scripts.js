270d79a7388e7d97711d76fc1febd1ef494c110f
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-03-04 16:07:34 -0500

    final commit for the day

--- assets/scripts.js
+++ assets/scripts.js
@@ -43,10 +43,41 @@ function addToCart(data, form, modal) {
 	})
 }
 
-
+//create a new function with ajax request to shipping rates endpoint
+function getShippingRates(data) {
+	$.ajax({
+	    Type: 'GET',
+	    url: '/cart/shipping_rates.json', 
+	    dataType: 'json', 
+	    data: {
+	        'shipping_address[zip]': data.zip, 
+	        'shipping_address[country]' : data.country, 
+	        'shipping_address[province]' : data.province
+	    }, 
+	        success: (res) => {
+	        console.log(res);
+	        //ANOTHER WAY OF DOING IT IS...
+	        // var i = 0;
+	        // for(i = 0; i < res.shipping_rates.length; i++) {
+	        // 	$('#shipping-estimate').append('<p>the estimated shipping cost for current items is $' + res.shipping_rates[i].price + '</p>')
+	        // 	console.log(res.shipping_rates[i].price)
+	        // }
+	        var shipping_container = $('<div>');
+	        res.shipping_rates.forEach(function(rate) {
+	        	// console.log(rate, 'this is it!');
+	        	// shipping_container.append('<p>the estimated shipping cost for current items for' + rate.name + 'is going to be' + rate.price + '</p>');
+	        	shipping_container.append(`<p>the estimated shipping cost for current items for ${rate.name} is going to be ${rate.price} </p>`);
+	        })
+	       	$('#shipping-estimate').html(shipping_container);
+	    },
+	        error: (err) => {
+	        console.log(err);
+	    }
+	})
+}
 
 $(function(){
-	
+
 	$('[data-modal]').on('click', function(){
 		modalOpen($(this).data('modal'))
 	});
@@ -71,6 +102,25 @@ $(function(){
 		addToCart(data, $form, modal)
 	})
 
+	$('#shipping-calculator').on('submit', function(e) {
+	        // prevent page refresh on form submit
+	        e.preventDefault()
+
+	        //select the form and serialize the form data
+	        var form = $(this)
+	        var form_data = form.serializeArray()
+	        var data = {};
+
+	        $(form_data).each(function(index, obj){
+	            data[obj.name] = obj.value;
+	        });
+
+	        // pass the form data to the getShippingRate function to make the Ajax request
+	        getShippingRates(data)
+
+	        $(form)[0].reset()
+
+	    });
 	
 })
 
